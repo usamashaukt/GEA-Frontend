2fcc1e7cbae2a4fb36d196899aa450ed34985711
usamashaukt <usamashaukat36@gmail.com>
2024-12-18 03:45:32 +0500

    lowercase h of home.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Nav from "./components/Nav";
 import "./App.css";
 import Footer from "./components/Footer";
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home/Home";
+import Home from "./pages/home/Home";
 import Universities from "./pages/Universities/Universities";
 import VisaApplication from "./pages/UnitedKingdom/VisaApplication";
 import Australia from "./pages/Australia/Australia";
