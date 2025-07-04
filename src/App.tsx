97801f70593b679f481661d2f1f2b3f9def5b56e
usamashaukt <usamashaukat36@gmail.com>
2025-07-04 19:51:59 +0500

    changed gea to hu

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,31 @@ import Nav from "./components/Nav";
 import "./App.css";
 import Footer from "./components/Footer";
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home/HomePage";
-import Universities from "./pages/Universities/Universities";
-import VisaApplication from "./pages/UnitedKingdom/VisaApplication";
-import Australia from "./pages/Australia/Australia";
-import Usa from "./pages/Usa/Usa";
-import Europe from "./pages/Europe/Europe";
+import { Suspense, lazy } from "react";
 import WhatsApp from "./components/WhatsApp";
+
+const Home = lazy(() => import("./pages/Home/HomePage"));
+const Universities = lazy(() => import("./pages/Universities/Universities"));
+const VisaApplication = lazy(() => import("./pages/UnitedKingdom/VisaApplication"));
+const Australia = lazy(() => import("./pages/Australia/Australia"));
+const Usa = lazy(() => import("./pages/Usa/Usa"));
+const Europe = lazy(() => import("./pages/Europe/Europe"));
 
 function App() {
   return (
     <>
       <div>
         <Nav />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/universities" element={<Universities />} />
-          <Route path="/aus" element={<Australia />} />
-          <Route path="/usa" element={<Usa />} />
-          <Route path="/eu" element={<Europe />} />
-          <Route path="/uk" element={<VisaApplication />} />
-        </Routes>
+        <Suspense fallback={<div style={{textAlign: 'center', marginTop: '3rem'}}>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/universities" element={<Universities />} />
+            <Route path="/aus" element={<Australia />} />
+            <Route path="/usa" element={<Usa />} />
+            <Route path="/eu" element={<Europe />} />
+            <Route path="/uk" element={<VisaApplication />} />
+          </Routes>
+        </Suspense>
         <WhatsApp phoneNumber="+923394007202 " />
         <Footer />
       </div>
