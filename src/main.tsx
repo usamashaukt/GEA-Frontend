b9c0e5efb20b30827e2f1199e7f12153e0c85427
usamashaukt <usamashaukat36@gmail.com>
2025-07-31 21:26:55 +0500

    CHanged styling of cta buttons

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import App from "./App.tsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
+import "./App.css"
 import * as React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
