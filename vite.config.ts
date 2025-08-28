d21fc87416817c1a8d11bc2dd7d49a6bddd85670
usamashaukt <usamashaukat36@gmail.com>
2025-08-28 05:37:35 +0500

    reduced unused js

--- vite.config.ts
+++ vite.config.ts
@@ -9,16 +9,26 @@ export default defineConfig({
       output: {
         manualChunks: {
           vendor: ['react', 'react-dom'],
+          router: ['react-router-dom'],
           bootstrap: ['bootstrap', 'react-bootstrap'],
-          icons: ['@fortawesome/fontawesome-svg-core', '@fortawesome/free-solid-svg-icons', '@fortawesome/free-brands-svg-icons', '@fortawesome/free-regular-svg-icons'],
+          icons: ['@fortawesome/fontawesome-svg-core', '@fortawesome/free-solid-svg-icons', '@fortawesome/free-brands-svg-icons', '@fortawesome/free-regular-svg-icons', 'react-icons'],
           carousel: ['react-slick', 'slick-carousel'],
           swiper: ['swiper'],
+          utils: ['jwt-decode', 'file-saver'],
         },
       },
     },
     chunkSizeWarningLimit: 1000,
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
   },
   optimizeDeps: {
     include: ['react', 'react-dom', 'react-router-dom'],
+    exclude: ['@fortawesome/fontawesome-svg-core'],
   },
 })
